8e3c0cb42778e92d2a9dc70b44be3169fae184c8
nzod <zodcode@gmail.com>
2015-02-08 23:34:22 +0100

    add missing EditorBar

---
+++ EditorBar.py
@@ -0,0 +1,19 @@
+#encoding: UTF-8
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+import glib
+
+
+class EditorBar(gtk.EventBox):   
+   def __init__(self):
+      gtk.EventBox.__init__(self)
+      
+      self.entry = gtk.Entry()
+      
+      box = gtk.HBox()
+      box.pack_start(self.entry, expand=True, padding=2)
+      self.add(box)
+      
+
